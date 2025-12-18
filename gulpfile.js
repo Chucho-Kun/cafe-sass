3ece5a7228835a21ca1a6c3cfd2abd38552aa3fb
Jesus Peralta <desarrolladorwebodm@gmail.com>
2025-12-18 14:46:29 -0600

    failed using imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,58 @@
-const { src, dest, watch , series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+import { src, dest, watch, series } from 'gulp';
 
-function css( done ) {
-    // compilar sass
+// CSS y SASS
+import gulpSass from 'gulp-sass';
+import * as sassCompiler from 'sass';
+const sass = gulpSass(sassCompiler);
+
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+
+// Imágenes
+import imagemin from 'gulp-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminOptipng from 'imagemin-optipng';
+import imageminSvgo from 'imagemin-svgo';
+import webp from 'gulp-webp';
+
+export function css(done) {
     src('src/scss/app.scss')
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]))
-        .pipe( dest('build/css') )
-    
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'));
     done();
 }
 
-function dev() {
-    watch( 'src/scss/**/*.scss', css );
+export function imagenes(done) {
+    src('src/img/**/*.{png,jpg,jpeg,svg}')
+        .pipe(imagemin([
+            imageminMozjpeg({ quality: 75, progressive: true }),
+            imageminOptipng({ optimizationLevel: 5 }), // mejor equilibrio
+            imageminSvgo({
+                plugins: [
+                    { name: 'removeViewBox', active: false },
+                    { name: 'cleanupIDs', active: false }
+                ]
+            })
+        ], {
+            verbose: true
+        }))
+        .pipe(dest('build/img'));
+    done();
 }
 
-function tareaDefault(){
-    console.log('Soy tarea por default....');
-    
+
+export function imgWebp(done) {
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(webp({ quality: 75, method: 6 }))
+        .pipe(dest('build/img'));
+    done();
 }
 
-exports.css = css;
-exports.dev = dev;
-exports.default = series( css, dev );
 
-// series - atiende una tarea despues de otra
-// parallel - todas las tareas inician al mismo tiempo
+export function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
+export default series(imgWebp, css, dev);
