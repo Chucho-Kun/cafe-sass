9f78db83d3bbbfe25b4c4e18ce64bb5994278218
Jesus Peralta <desarrolladorwebodm@gmail.com>
2025-12-18 16:33:55 -0600

    convert images to webp in the same folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,33 @@
 import { src, dest, watch, series } from 'gulp';
-
-// CSS y SASS
 import gulpSass from 'gulp-sass';
 import * as sassCompiler from 'sass';
 const sass = gulpSass(sassCompiler);
-
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import shell from 'gulp-shell';
 
-// Imágenes
-import imagemin from 'gulp-imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
-import imageminOptipng from 'imagemin-optipng';
-import imageminSvgo from 'imagemin-svgo';
-import webp from 'gulp-webp';
-
-export function css(done) {
-    src('src/scss/app.scss')
+// CSS
+export function css() {
+    return src('src/scss/app.scss')
         .pipe(sass())
         .pipe(postcss([autoprefixer()]))
         .pipe(dest('build/css'));
-    done();
 }
 
-export function imagenes(done) {
-    src('src/img/**/*.{png,jpg,jpeg,svg}')
-        .pipe(imagemin([
-            imageminMozjpeg({ quality: 75, progressive: true }),
-            imageminOptipng({ optimizationLevel: 5 }), // mejor equilibrio
-            imageminSvgo({
-                plugins: [
-                    { name: 'removeViewBox', active: false },
-                    { name: 'cleanupIDs', active: false }
-                ]
-            })
-        ], {
-            verbose: true
-        }))
-        .pipe(dest('build/img'));
-    done();
+// Optimización imágenes con cwebp
+export function imgWebp() {
+    return src('src/img/**/*.{jpg,jpeg,png}', { read: false })
+        .pipe(shell([
+            'cwebp "<%= file.path %>" -q 70 -o "build/img/<%= file.relative.replace(/\\.[^.]+$/, ".webp") %>"'
+        ], { ignoreErrors: true }))
+        // no uses dest(), porque no hay buffer que copiar
 }
 
 
-export function imgWebp(done) {
-    src('src/img/**/*.{png,jpg,jpeg}')
-        .pipe(webp({ quality: 75, method: 6 }))
-        .pipe(dest('build/img'));
-    done();
-}
-
-
+// Watcher
 export function dev() {
     watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
+    watch('src/img/**/*.{jpg,jpeg,png}', imgWebp);
 }
 
-export default series(imgWebp, css, dev);
+export default series(css, imgWebp, dev);
